909215cf79683ac4dc8c54593ee400e043711bfa
isha661 <ishaluhun@gmail.com>
2026-06-08 12:19:41 +0545

    Added python basic

---
+++ python_b1.py
@@ -0,0 +1,44 @@
+# #basic data print 
+print("Hello this is isha learning python from scracth to mastery")
+
+#datatype 
+
+print()
+print("Different data types in python")
+a=10
+b=10.5
+c="Isha"
+d=True 
+
+print(a) # interger type
+print(b) # float type
+print(c) # string type
+print(d) # boolean type
+print()
+
+# #operation
+print("Different operations in python")
+print()
+print(3+3) # addition
+print(10-5)# subtraction
+print(4*5) # multiplication
+print(10/2) # division
+print(10%3) # modulus
+print(10 * 'isha') # string multiplication
+
+print()
+# #varaiable 
+print(" Varaible in python")
+test = 123
+test = test+1
+print(test )
+print()
+
+#input
+print("INPUT IN PYTHON")
+print()
+print("Fill the name and age")
+
+name = input("ENTER YOUR NAME: ")
+age = input("ENTER YOUR AGE: ")
+print(" Hello {name} thank you, for providing your details , have a good day!")

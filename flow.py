2da1fa31b5b789cd40f42bd3cc31a6f26b14c8ee
isha661 <ishaluhun@gmail.com>
2026-06-09 16:13:21 +0545

    control flow in python

---
+++ flow.py
@@ -0,0 +1,33 @@
+#Flow Control in Python 
+
+#checking gretaer value using if statement
+# a = 10
+# b = 27
+# c = 15
+# if a > b and a > c:
+#     print("a is greater than b and c")
+    
+# elif b > a and b > c:
+#     print("b is greater than a and  c")
+
+# else:
+#     print("c is greater than a and b")
+#     print()
+
+
+
+# While loop
+
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 1     #this is incrementing the value of i by 1 in each iteration 
+# print("while loop is over")  
+
+
+# for loop
+
+test_list = [1,2,3,4,5]
+for i in test_list:
+    print(i)
+

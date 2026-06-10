a870834947f59a52aa8674d7800399c099162581
isha661 <ishaluhun@gmail.com>
2026-06-10 21:57:46 +0545

    inheritance in class

--- classes.py
+++ classes.py
@@ -18,28 +18,97 @@
 
 
 #creating def function in class  /////////////////////////////////////////////////////////////////////// 
-class IshaClass:
-    my_name = 'Isha'
-    my_age = 20
+# varaibales in class is attribute
+#functions in a class are methods
 
-    def isha_fun(self): # self refers to any instance of the class and must be the first parameter for all methods
-        print("isha function is called in class now")
-        print(self.my_name)
-        self.another_fun("here in isha funcction we call another fun")
+# class IshaClass:
+#     my_name = 'Isha'
+#     my_age = 20
+
+#     def isha_fun(self): # self refers to any instance of the class and must be the first parameter for all methods
+#         print("isha function is called in class now")
+#         print(self.my_name)
+#         self.another_fun("here in isha funcction we call another fun")
     
 
-    #calling function
-    # print("lets call isha function in class")
-    # isha_fun()
+#     def another_fun(self, para):
+#         print(para)
 
-    def another_fun(self, para):
-        print(para)
-
-#creating an instance
-my = IshaClass()
-print(my.my_name)
-print(my.my_age)    
-my.isha_fun()
-my.another_fun("This is another new function") #this is for another function
+# #creating an instance
+# my = IshaClass()
+# print(my.my_name)
+# print(my.my_age)    
+# my.isha_fun()
+# my.another_fun("This is another new function") #this is for another function
 
 
+
+#Dunder (double underscore) method are special methods of a class
+# __int __is run when an instance of the class is created
+# __len__ gets called when the instance is passed into the len function 
+
+#Mage class
+# class Mage:
+#     def __init__(self, health, mana):
+#         self.health = health
+#         self.mana = mana
+#         print("the mage class was created")
+#         print(self.health)
+#     def __len__(self):
+#         return self.mana    
+       
+
+# mage = Mage(100, 200)
+# print(len(mage))
+
+
+
+# class Mage:
+#     def __init__(self, health, mana):
+#         self.health = health
+#         self.mana = mana
+#         print("the mage class was created")
+#         print(self.health)
+#     def attack(self, target):
+#         target.health -= 10
+
+# class Monster:
+#     health = 40
+       
+
+# mage = Mage(100, 200)
+# monster = Monster()
+
+# print(monster.health)
+# mage.attack(monster)
+# print(monster.health)
+
+
+
+
+#Inheritance (one class can get the method and attributes of another class)
+class Human:
+     
+     def __init__(self, health):
+          self.health = health
+          
+     def attack(self):
+          print("attack")
+
+
+class Warrior(Human):
+    def __init__(self, health, defense):
+          super().__init__(health) #super this one call parent class
+          self.defense = defense
+      
+class Barbarian(Human):
+    def __init__(self, health, damage):
+          super().__init__(health)
+          self.damage = damage
+
+warrior = Warrior(50,5.5)
+barbarian = Barbarian(100, 8.1)
+warrior.attack()
+barbarian.attack()
+print(warrior.health)
+print(warrior.defense)

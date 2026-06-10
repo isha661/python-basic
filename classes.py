4df093729b61fd5b3bc57d5bc4778658ff74af1b
isha661 <ishaluhun@gmail.com>
2026-06-10 20:33:10 +0545

    creating classes

--- classes.py
+++ classes.py
@@ -1,3 +1,13 @@
 # Classes are a way to organize code
 # Basic class
 
+class TestClass:
+    test_var = "hello"
+    next_var = [1,2,3,4,]
+
+# creating an instance by calling it
+test = TestClass() #test is instance 
+print(test.test_var)
+print(test.next_var)
+
+

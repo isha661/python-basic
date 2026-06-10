4348c6a889a9a4980e6c43f2f56143085dfba35b
isha661 <ishaluhun@gmail.com>
2026-06-11 00:17:59 +0545

    importing python functionaliyt

---
+++ modules.py
@@ -0,0 +1,7 @@
+# it provide extra functionalities via the puython standard library
+# can help to organize code via multiple files
+
+import math 
+import random # gives random num
+
+print(random.randint(0,10))

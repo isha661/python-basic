5df68c365ab5138547b6acd0f61132389afe7a6d
isha661 <ishaluhun@gmail.com>
2026-06-10 14:17:07 +0545

    creatinng function and calling for hypotenuse of triangle

--- function.py
+++ function.py
@@ -35,12 +35,29 @@
 
 
 #example of function with loop  and returing loop value count
-def print_x_times(paramter, loop_amount = 5):
-    count = 0
-    while count <= loop_amount:
-     print(count, paramter)
-     count += 1
-    return f'the loop amount is {loop_amount}'
-print('isha')
-test = print_x_times('exam') 
-print(test)   # we can add loop amount here to ('exma', 2)
+# def print_x_times(paramter, loop_amount = 5):
+#     count = 0
+#     print(global_var)
+#     while count <= loop_amount:
+#      print(count, paramter)
+#      count += 1
+#     return f'the loop amount is {loop_amount}'
+# print('isha')
+# global_var = 'global variable'
+# test = print_x_times('exam') 
+# print(test)   # we can add loop amount here to ('exma', 2)
+
+
+#global varaibale : this should be avaiable for every  function
+
+
+#creaating function
+#Excercise: creating a pythagoras theorem calculator once it run it should ask the user for 2 num that are hight and width of a traingle it should output the length of the hypotenuse.
+def hypotenuse_calculator(side_a, side_b):
+ hypotenuse = pow(side_a**2 + side_b**2, 0.5) #pow is a built in function which is used to calculate the power of a number
+ return round(hypotenuse, 2)
+
+#calling 
+print(hypotenuse_calculator(1,1))
+
+

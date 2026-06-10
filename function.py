d7da7935ef8a06174445145cd7137b0742dc05c8
isha661 <ishaluhun@gmail.com>
2026-06-10 13:06:31 +0545

    function with parameter and loop

--- function.py
+++ function.py
@@ -1,21 +1,31 @@
-# creating a function 
-def print_5_times():
-    print("Today i am learning about function and how to call the function")
-    print()
-# call 
-print('Good morning everyone this is isha third day of learning python')
-print()
-print_5_times()
+# # creating a function 
+# def print_5_times():
+#     print("Today i am learning about function and how to call the function")
+#     print()
+# # call 
+# print('Good morning everyone this is isha third day of learning python')
+# print()
+# print_5_times()
 
 
-#next example  
-def print_6_times():
-    isha = 0
-    while isha <= 6:
-        print("isha")
-        isha += 1
+# #next example  
+# def print_6_times():
+#     isha = 0
+#     while isha <= 6:
+#         print("isha")
+#         isha += 1
 
-# call 
-print('lets print the isha 6 times')
-print()
-print_6_times()
+# # call 
+# print('lets print the isha 6 times')
+# print()
+# print_6_times()
+
+#example of function with loop 
+def print_x_times(paramter, loop_amount):
+    count = 0
+    while count <= loop_amount:
+     print(count, paramter)
+     count += 1
+#call
+print('isha')
+print_x_times('exam',2)    

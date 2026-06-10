a9f04ec4851bce7bcabdd5a027a96441ae17c981
isha661 <ishaluhun@gmail.com>
2026-06-10 12:50:52 +0545

    learning function and calling the function

---
+++ function.py
@@ -0,0 +1,18 @@
+# creating a function 
+def print_5_times():
+    print("hello this is function")
+# call 
+print('print')
+print_5_times()
+
+
+#next example  
+def print_6_times():
+    isha = 0
+    while isha <= 6:
+        print("isha")
+        isha += 1
+
+# call 
+print('print')
+print_6_times()
